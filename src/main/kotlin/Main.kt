dcc676003001bbb8994a536035f002b621e17fe3
alsomeb <alsomeb@gmail.com>
2023-02-11 18:12:55 +0100

    added small improvements day2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -36,10 +36,64 @@ fun partTwo(list: List<String>): Int {
     return score
 }
 
+// EFTER, Todd Ginsberg
+
+// den bästa jag hittade som jag tyckte var bra
+// han har gjort nästan exakt samma som mig men istället använt sumOf() för att räkna ut resultat
+// Då kan man sätta score variabel som "val" även pga den behöver ej initieras förens den summeras
+
+// The sumOf method of List applies the provided selector function on each element and returns the sum of the values returned by the selector function for each element of the List.
+// https://www.educative.io/answers/how-to-use-the-sumof-method-of-list-in-kotlin
+fun partOneImproved(list: List<String>): Int {
+    val score: Int // Vi kan ha val, för den assignas när vi summerar nedan och sen blir immutable
+    val map = mapOf(
+        "AY" to 8,
+        "AX" to 4,
+        "AZ" to 3,
+        "BX" to 1,
+        "BY" to 5,
+        "BZ" to 9,
+        "CX" to 7,
+        "CY" to 2,
+        "CZ" to 6)
+
+    // it är ett keyword i kotlin
+    // "Whenever you have a function literal with exactly one parameter
+    // you don’t have to define the parameter explicitly but you can just use 'it'"
+    score = list.sumOf { map[it]!!}
+    return score
+}
+
+fun partTwoImproved(list: List<String>): Int {
+    val score: Int  // Vi kan ha val, för den assignas när vi summerar nedan och sen blir immutable
+    val map = mapOf(
+        "AY" to 4,
+        "AX" to 3,
+        "AZ" to 8,
+        "BX" to 1,
+        "BY" to 5,
+        "BZ" to 9,
+        "CX" to 2,
+        "CY" to 6,
+        "CZ" to 7)
+
+    score = list.sumOf { map[it]!!}
+    return score
+}
+
 
 fun main() {
+    // FÖRE
     val input = getAoCList()
     val result1 = partOne(input)
     val result2 = partTwo(input)
     println("Part One: $result1\nPart two: $result2")
+
+    // EFTER
+    val result1Improved = partOneImproved(input)
+    val result2Improved = partTwoImproved(input)
+    println("Part One Imp: $result1Improved\nPart two Imp: $result2Improved")
+
+
+
 }
