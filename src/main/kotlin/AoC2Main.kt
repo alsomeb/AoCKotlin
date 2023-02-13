e4edf2356c43b95d0fbed4be431ef5373fc32f0e
alsomeb <alsomeb@gmail.com>
2023-02-13 09:30:00 +0100

    Completed 2021 dag 2 med extra

--- src/main/kotlin/AoC2Main.kt
+++ src/main/kotlin/AoC2Main.kt
@@ -52,8 +52,62 @@ fun partTwo() {
     println("Result2 =  ${totalDepth * totalHorizontal}")
 }
 
+// EFTER MHA https://github.com/asm0dey/aoc-2021/blob/main/src/Day02.kt
+// Denna var intressant för man kunde korta ner koden och använda .map för att göra smålistor i en lista
+// sedan loopa igenom direction, amount
+// Det mest intressanta var funktionen check() som är som ett mini test, se impl längre ner i main
+
+fun part1Improved(input: List<String>): Int {
+    var horiz = 0
+    var depth = 0
+    val instructions = input.map { it.split(' ') }
+
+    for((dir, amountString) in instructions) {
+        val amount = amountString.toInt()
+        when (dir) {
+            "forward" -> horiz += amount
+            "up" -> depth -= amount
+            "down" -> depth += amount
+        }
+    }
+
+    return horiz * depth
+}
+
+fun part2Improved(input: List<String>): Int {
+    var horiz = 0
+    var depth = 0
+    var aim = 0
+    val instructions = input.map { it.split(' ') }
+
+    for((dir, amountString) in instructions) {
+        val amount = amountString.toInt()
+        when (dir) {
+            "forward" -> {
+                horiz += amount
+                depth += (aim * amount)
+            }
+            "up" -> aim -= amount
+            "down" -> aim += amount
+        }
+    }
+
+    return horiz * depth
+}
+
 
 fun main() {
+    // FÖRE
     partOne()
     partTwo()
+
+    // EFTER
+    val input = getInput()
+    val res1 = part1Improved(input)
+    val res2 = part2Improved(input)
+
+    // Mini tests
+    // Throws an IllegalStateException if the value is false
+    check(res1 == 2039256)
+    check(res2 == 1856459736)
 }
